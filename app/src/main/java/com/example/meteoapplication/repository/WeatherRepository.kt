74dfd310baf16692cc5e2674cc3c6bc347b12b7b
Sam <bertrand.sam78@gmail.com>
2021-05-12 00:33:28 +0200

    commentaire

--- app/src/main/java/com/example/meteoapplication/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/meteoapplication/repository/WeatherRepository.kt
@@ -3,6 +3,8 @@ package com.example.meteoapplication.repository
 import com.example.meteoapplication.retrofit.RetrofitClient
 
 class WeatherRepository {
+
+    // Raccourci pris l'APIKEY ne devrait pas se trouver ici normalement
     private val apiKey = "6820fcda05c55b7b2733ea210e13b0a9"
 
     suspend fun getWeatherFromIdCitiesList(cityId: String) =
